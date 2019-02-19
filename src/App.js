cbc4585042733d66f1602274b8fc5d46b26269cb
Ulanbek Zhusupov <diz.dostuk@yandex.ru>
2019-02-19 23:11:14 +0300

    Rewrited redux lesson1

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import { combineReducers } from 'redux'
-import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
+import { combineReducers } from 'redux';
+import { VisibilityFilters, SET_VISIBILITY_FILTER, TOGGLE_TODO, ADD_TODO } from './actions';
+
 const { SHOW_ALL } = VisibilityFilters;
 
 function visibilityFilter(state = SHOW_ALL, action) {
-  switch (action.type) {
+  switch(action.type) {
     case SET_VISIBILITY_FILTER:
-      return action.filter
-    default:
-      return state
+      return action.filter;
+    default: return state;
   }
 }
 
 function todos(state = [], action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return [
-        ...state,
+  switch(action.type) {
+    case ADD_TODO: return Object.assign({}, state, {
+      todos: [
+        ...state.todos,
         {
           text: action.text,
           completed: false
         }
       ]
+    });
     case TOGGLE_TODO:
-      return state.map((todo, index) => {
-        if (index === action.index) {
-          return Object.assign({}, todo, {
-            completed: !todo.completed
-          })
-        }
-        return todo
-      })
-    default:
-      return state
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if(index === action.index) {
+            return Object.assign({}, state, {
+              completed: !todo.completed
+            });
+          }
+          return todo;
+        })
+      });
+    default: return state;
   }
 }
+
+
 
 const todoApp = combineReducers({
   visibilityFilter,
   todos
-})
+});
 
-export default todoApp
+export default todoApp;
