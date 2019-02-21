ca39ea61fc4b0bf6dd41f491666247e9a556f3ef
Ulan <diz.dostuk@yandex.ru>
2019-02-21 21:28:33 +0330

    Deleted all previous files, and create new with new lesson from MonsterLessons

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import Footer from "./components/Footer";
-import AddTodo from "./containers/AddTodo";
-import VisibleTodoList from "./containers/VisibleTodoList";
+import React from "react";
+import { createStore } from "redux";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <AddTodo/>
-        <VisibleTodoList/>
-        <Footer/>
-      </div>
-    );
+
+function playlist(state = [], action) {
+  if(action.type === 'ADD_TRACK') {
+    return [
+      ...state,
+      action.payload
+    ]
   }
+  return state;
 }
+
+const store = createStore(playlist);
+
+const btn = document.getElementById("button");
+const list = document.querySelectorAll(".list")[0];
+const trackInput = document.querySelectorAll(".trackInp")[0];
+
+
+
+store.subscribe(() => {
+  list.innerHTML = '';
+  trackInput.value = '';
+  store.getState().forEach(track => {
+    const li = document.createElement("li");
+    li.textContent = track;
+    list.appendChild(li);
+  })
+})
+
+btn.addEventListener("click", () => {
+  const trackName = trackInput.value;
+  store.dispatch({ type: 'ADD_TRACK', payload: trackName});
+})
+
+const App = () => {
+  return (
+    <div>
+      <input type="text" className="trackInp" />
+      <button id="button">Submit</button>
+      <ul className="list" />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import todoApp from "./reduxjs/todoApp";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let store = createStore(todoApp);
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>, document.getElementById('root'));
+    <App />,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
